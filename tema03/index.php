bbf8ce6e97981fba3f369ee556831bbac6c6aff2
AlemolAMG02 <amolgom25@iesmarquesdecomares.org>
2021-10-20 10:32:02 +0200

    Conceptos básicos conexión a servidor

--- tema03/index.php
+++ tema03/index.php
@@ -1,11 +1,16 @@
 <?php
-    /* Conexión por clases  */
-//    $dwes = new mysqli('localhost','dwes','abc123.','dwes');
-//    echo $dwes->server_info;
-    
     /* Conexión por métodos procedimentales */
-    $dwes = mysqli_connect('localhost','dwes','abc123.','dwes');
-    echo mysqli_get_server_info($dwes);
+//    $dwes = mysqli_connect('localhost','dwes','abc123.','dwes');
+//    echo mysqli_get_server_info($dwes);
+
+/* Conexión por clases  */
+    $dwes = new mysqli('localhost','dwe','abc123.','dwes'); //Está mal a propósito
+    if (!$dwes->connect_errno){     //Entra si no hay errores
+            echo $dwes->server_info;
+            echo '<br>';
+    } else {
+        echo $dwes->connect_errno."-".$dwes->connect_error;
+    }    
 
 ?>
 
