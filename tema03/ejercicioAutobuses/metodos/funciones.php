71b7bdbcda500d3fc0dea981c15563a5c2f7e63f
AlemolAMG02 <amolgom25@iesmarquesdecomares.org>
2021-11-11 19:49:30 +0100

    UP Ejercicio autobuses

--- tema03/ejercicioAutobuses/metodos/funciones.php
+++ tema03/ejercicioAutobuses/metodos/funciones.php
@@ -2,21 +2,11 @@
 function conexionBD()
 {
     $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
-    $dwes = new PDO('mysql:host=localhost; dbname=dwes; charset=utf8mb4', 'dwes', 'abc123.', $options);
+    $dwes = new PDO('mysql:host=localhost; dbname=autobuses; charset=utf8mb4', 'dwes', 'abc123.', $options);
     if ($dwes->errorCode() != 0) {
         print_r($dwes->errorInfo());
         die("ERROR");
     }
-
-    /*If ($dwes->connect_errno) {     //Entra si hay hay errores
-            //$mensaje = $dwes->connect_errno."-".$dwes->connect_error;
-            $error = $dwes->connect_errno;
-            die("ERROR AL CONECTAR CON EL SERVIDOR DE BD");
-        }
-        $error = $dwes->connect_errno;
-        $mensaje = "No hay errores";
-        //echo "<h3>ERROR $error : $mensaje </h3>";
-    */
     return $dwes;
 }
 
@@ -28,17 +18,17 @@ function muestraTexto($texto)
 function calcOrigen($conx)
 {
     $sql = "SELECT DISTINCT `Origen` FROM `viajes` ORDER BY `Origen`;";
-    //echo "Texto inicio";
+    echo "Texto inicio";
     $result = $conx->query($sql);
     if ($conx->errorCode() != 0) {
-        //print_r($conx->errorInfo());
+        print_r($conx->errorInfo());
         die("ERROR");
     }
     //echo 'Hago la query';
     while ($a = $result->fetch()) {
-        /*if ($_POST['origen'] == $a->Origen) $coinc =  'selected';
-        else*/
-        $coinc = "";
+        if ($_POST['origen'] == $a) $coinc =  'selected';
+        else
+            $coinc = "";
         echo "<option value='$a->Origen' $coinc >$a->Origen</option><br>";
     }
 }
@@ -53,5 +43,44 @@ function calcDestino($conx)
         if ($_POST['destino'] == $a->Destino) $coinc =  'selected';
         else $coinc = "";
         echo "<option value='$a->Destino' $coinc >$a->Destino</option><br>";
+    }
+}
+
+function calcFecha($conx)
+{
+    $sql = "SELECT DISTINCT `Fecha` FROM `viajes` ORDER BY `Fecha`;";
+    //echo "Texto inicio";
+    $result = $conx->query($sql);
+    if ($conx->errorCode() != 0) {
+        print_r($conx->errorInfo());
+        die("ERROR");
+    }
+    while ($a = $result->fetch()) {
+        if ($_POST['fecha'] == $a->Fecha) $coinc =  'selected';
+        else
+            $coinc = "";
+        echo "<option value='$a->Fecha' $coinc >$a->Fecha</option><br>";
+    }
+}
+
+function calcViaje($conx, $fecha, $origen, $destino)
+{
+    $sql = "SELECT * FROM viajes WHERE `Fecha` = '$fecha' AND `Origen` = '$origen' AND `Destino` = '$destino' LIMIT 1;";
+    $result = $conx->query($sql);
+    if ($conx->errorCode() != 0) {
+        print_r($conx->errorInfo());
+        return false;
+        die("ERROR");
+    }
+
+    return $result->fetch();
+}
+
+function verifViaje($query)
+{
+    if ($query->fetch()) {
+        return true;
+    } else {
+        return false;
     }
 }
