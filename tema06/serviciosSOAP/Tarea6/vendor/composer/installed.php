25b59b755eb4acbdab6104e7fecb1bfa26763a56
AlemolAMG02 <amolgom25@iesmarquesdecomares.org>
2022-01-12 08:55:22 +0100

    ADD Nuevo ejercicio

---
+++ tema06/serviciosSOAP/Tarea6/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '851a5f455f67eec0921afbd4a19f9758da6e8d19',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '851a5f455f67eec0921afbd4a19f9758da6e8d19',
+            'dev_requirement' => false,
+        ),
+        'econea/nusoap' => array(
+            'pretty_version' => 'v0.9.11',
+            'version' => '0.9.11.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../econea/nusoap',
+            'aliases' => array(),
+            'reference' => '2e00c785f3a0c4bb12f3fe818fbb6b3064bd045e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
