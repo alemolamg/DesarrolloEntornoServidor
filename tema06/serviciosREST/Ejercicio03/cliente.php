0e00da2a1dd3442b3a09bc718d31776a8d7812bf
AlemolAMG02 <amolgom25@iesmarquesdecomares.org>
2022-01-16 14:30:33 +0100

    up Server preparado, pero no contempla errores

--- tema06/serviciosREST/Ejercicio03/cliente.php
+++ tema06/serviciosREST/Ejercicio03/cliente.php
@@ -1,36 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 03 Cambio Monedas</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicio 03 Cambio Monedas</title>
+    </head>
 
-<body>
-    <div id="principal">
-        <h1>Calcula tu Cambio de Divisas</h1>
-        <form action="." method="get">
-            <label for="monedaPrincipal">Moneda Principal: </label>
-            <input type="number" name="monedaPrincipal" id="monedaPrincipal">
-            <select name="tipoPrincipal" id="tipoPrincipal">
-                <option value="libra">Libras</option>
-                <option value="euro" selected>Euros</option>
-                <option value="dolar">Dolares</option>
-            </select>
-            <br>
-            <label for="nuevaMoneda">Cambiar moneda a:</label>
-            <select name="nuevaMoneda" id="nuevaMoneda">
-                <option value="libra">Libras</option>
-                <option value="euro">Euros</option>
-                <option value="dolar" selected>Dolares</option>
-            </select>
-            <input type="submit" value="Enviar">
-        </form>
+    <body>
+        <div id="principal">
+            <h1>Calcula tu Cambio de Divisas</h1>
+            <form action="." method="post">
+                <label for="cantidad">Moneda Principal: </label>
+                <input type="number" name="cantidad" id="cantidad">
+                <select name="tipoPrincipal" id="tipoPrincipal">
+                    <option value="libra">Libras</option>
+                    <option value="euro" selected>Euros</option>
+                    <option value="dolar">Dolares</option>
+                </select>
+                <br>
+                <label for="nuevaMoneda">Cambiar moneda a:</label>
+                <select name="nuevaMoneda" id="nuevaMoneda">
+                    <option value="libra">Libras</option>
+                    <option value="euro">Euros</option>
+                    <option value="dolar" selected>Dolares</option>
+                </select>
+                <input type="submit" value="Enviar" name="enviar" id="enviar">
+            </form>
 
+        </div>
+        <br>
+        <hr>
+        
+        <?php
+        if (isset($_POST['enviar'])) {
+            echo "<h2>Calculo del cambio:</h2>";
+            $datos = file_get_contents("http://localhost/DesarrolloEntornoServidor/tema06/serviciosREST/Ejercicio03/servicio.php?orig=$_POST[monedaPrincipal]&dest=$_POST[tipoPrincipal]&cant=$_POST[cantidad]");
+            //$datos = explode("=", $datos, PHP_INT_MAX); // Divido el string en dos partes
+            //$datos = $datos[1];     // Me quedo con la parte codificada en JSON
+            echo '$datos';
+        }
 
-    </div>
-</body>
+        //calcularDecimo($_POST['numero'], $datos);
+        ?>
+    </body>
 
 </html>
--- tema06/serviciosREST/Ejercicio03/servicio.php
+++ tema06/serviciosREST/Ejercicio03/servicio.php
@@ -1,11 +1,53 @@
 <?php
-$divisaOrigen = $_GET['origen'];
-$divisaDestino = $_GET['destino'];
-$cantidad = $_GET['cant'];
+// Defino las variables con el valor de cada moneda
+define('EURO', 'EURO');
+define('DOLAR', 'DOLAR');
+define('LIBRA', 'LIBRA');
+define('EUROtoDOLAR', 1.14);
+define('EUROtoLIBRA', 0.83);
+define('DOLARtoLIBRA', 0.73);
+define('DOLARtoEURO', 0.88);
+define('LIBRAtoEURO', 1.20);
+define('LIBRAtoDOLAR', 1.37);
 
-$kk = array ('origen' => $divisaOrigen,'destino' => $divisaDestino,'cantidad' => $cantidad);
+$divisaOrigen = $_GET['orig'];  //divisa de origen
+$divisaDestino = $_GET['dest']; //divisa de destino
+$cantidad = $_GET['cant'];      //cantidad a cambiar
 
-echo json_encode($kk);
+$cambio = cambiarMoneda($divisaOrigen, $divisaDestino, $cantidad);
+if($cambio != 'error'){
+    echo json_encode($cambio);
+} else
+    echo $cambio;
 
+
+//echo json_encode($kk);
+
+function cambiarMoneda($divisaOrig, $divisaDest, $cant) {
+    $newCant = 0;
+    
+    if ($divisaOrig == EURO) {
+        if ($divisaDest == DOLAR) {
+            $newCant = $cant * EUROtoDOLAR;
+        } elseif ($divisaDest == LIBRA) {
+            $newCant = $cant * EUROtoLIBRA;
+        }
+    } elseif ($divisaOrig == DOLAR) {
+        if ($divisaDest == EURO) {
+            $newCant = $cant * DOLARtoEURO;
+        } elseif ($divisaDest == LIBRA) {
+            $newCant = $cant * DOLARtoLIBRA;
+        }
+    } elseif ($divisaOrig == LIBRA) {
+        if ($divisaDest == EURO) {
+            $newCant = $cant * LIBRAtoEURO;
+        } elseif ($divisaDest == DOLAR) {
+            $newCant = $cant * LIBRAtoDOLAR;
+        }
+    } else {
+        return 'error';
+    }
+    return array('origen' =>$divisaOrig, 'destino' => $divisaDest, 'cantidad' => $newCant);
+}
 
 ?>
